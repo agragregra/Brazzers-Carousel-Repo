c9c84c93a4d4e87056235a81f4e7ac44280a7788
Алексей <agragregra@ya.ru>
2025-02-09 16:19:36 +0300

    Update

--- Brazzers-Carousel/jQuery.Brazzers-Carousel.js
+++ Brazzers-Carousel/jQuery.Brazzers-Carousel.js
@@ -3,27 +3,27 @@
  * Copyright 2015 WebDesign Master.
  */
 (function($) {
-	$.fn.brazzersCarousel = function() {
-		return this.addClass("brazzers-daddy").append("<div class='tmb-wrap'><div class='tmb-wrap-table'>").append("<div class='image-wrap'>").each(function() {
-			var this_wrapper = $(this);
-			this_wrapper.find("img").appendTo(this_wrapper.find(".image-wrap")).each(function() {
-				this_wrapper.find(".tmb-wrap-table").append("<div>");
-			});
-		}).find(".tmb-wrap-table").bind('touchmove', function(event) {
-			event.preventDefault();
-			var myLocation = event.originalEvent.changedTouches[0];
-			var realTarget = document.elementFromPoint(myLocation.clientX, myLocation.clientY);
-			var this_img = $(realTarget).parent(".tmb-wrap-table").closest(".brazzers-daddy").find("img");
-			var all_thmbs = $(realTarget).parent(".tmb-wrap-table").find("div");
-			this_img.hide().eq($(realTarget).index()).css("display", "block");
-			all_thmbs.removeClass("active");
-			$(realTarget).addClass("active");
-		}).find("div").hover(function() {
-			var this_img = $(this).parent(".tmb-wrap-table").closest(".brazzers-daddy").find("img");
-			var all_thmbs = $(this).parent(".tmb-wrap-table").find("div");
-			this_img.hide().eq($(this).index()).css("display", "block");
-			all_thmbs.removeClass("active");
-			$(this).addClass("active");
-		}).parent().find(":first").addClass("active");
-	};
+  $.fn.brazzersCarousel = function() {
+    return this.addClass("brazzers-daddy").append("<div class='tmb-wrap'><div class='tmb-wrap-table'>").append("<div class='image-wrap'>").each(function() {
+      var this_wrapper = $(this);
+      this_wrapper.find("img").appendTo(this_wrapper.find(".image-wrap")).each(function() {
+        this_wrapper.find(".tmb-wrap-table").append("<div>");
+      });
+    }).find(".tmb-wrap-table").bind('touchmove', function(event) {
+      event.preventDefault();
+      var myLocation = event.originalEvent.changedTouches[0];
+      var realTarget = document.elementFromPoint(myLocation.clientX, myLocation.clientY);
+      var this_img = $(realTarget).parent(".tmb-wrap-table").closest(".brazzers-daddy").find("img");
+      var all_thmbs = $(realTarget).parent(".tmb-wrap-table").find("div");
+      this_img.hide().eq($(realTarget).index()).css("display", "block");
+      all_thmbs.removeClass("active");
+      $(realTarget).addClass("active");
+    }).find("div").hover(function() {
+      var this_img = $(this).parent(".tmb-wrap-table").closest(".brazzers-daddy").find("img");
+      var all_thmbs = $(this).parent(".tmb-wrap-table").find("div");
+      this_img.hide().eq($(this).index()).css("display", "block");
+      all_thmbs.removeClass("active");
+      $(this).addClass("active");
+    }).parent().find(":first").addClass("active");
+  };
 })(jQuery);
--- readme.md
+++ readme.md
@@ -4,14 +4,14 @@
 
 <h2>How To Use Plugin</h2>
 
-1) Plug in CSS: <code>&lt;link rel="stylesheet" href="css/jQuery.Brazzers-Carousel.min.css"&gt;</code> <br><br>			
+1) Plug in CSS: <code>&lt;link rel="stylesheet" href="css/jQuery.Brazzers-Carousel.min.css"&gt;</code> <br><br>      
 2) Plug in jQuery: <code>&lt;script href="js/jQuery.js"&gt;&lt;/script&gt;</code> <br><br>
 3) Plug in Brazzers Carousel JS: <code>&lt;script href="js/jQuery.Brazzers-Carousel.min.js"&gt;&lt;/script&gt;</code> <br><br>
 4) Add HTML Markup with images of the same proportions: <br>
 <pre>&lt;span class="thumb-item"&gt;
-	&lt;img src="img/image-1.jpg" alt="Alt"&gt;
-	&lt;img src="img/image-2.jpg" alt="Alt"&gt;
-	&lt;img src="img/image-3.jpg" alt="Alt"&gt;
+  &lt;img src="img/image-1.jpg" alt="Alt"&gt;
+  &lt;img src="img/image-2.jpg" alt="Alt"&gt;
+  &lt;img src="img/image-3.jpg" alt="Alt"&gt;
 &lt;/span&gt;</pre> <br>
 5) Start the plug-in:
 <pre>$(".thumb-item").brazzersCarousel();
